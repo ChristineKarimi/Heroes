acf3ea58011e5822efa48ed13172935dd64e5b20
christinekarimi <karimikim3@gmail.com>
2018-04-29 00:16:35 +0300

    Generated a new component and Added a class that lists the hero's properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+  
   ],
   imports: [
     BrowserModule
