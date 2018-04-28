d2a6cd8d5e8689f9f5858d30660a88fa216e3eca
christinekarimi <karimikim3@gmail.com>
2018-04-29 02:31:33 +0300

    Generated a new component to hold details of each hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -11,6 +12,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
